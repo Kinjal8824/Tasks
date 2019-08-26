c669962a6bd3b06cd323a4c99c090d800ff8f0e1
Kinjal8824 <54515668+Kinjal8824@users.noreply.github.com>
2019-08-25 21:42:04 -0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,6 @@
+curmax= 0
+lst=[2,15,5]
+for i in lst:
+    if (i>curmax):
+        curmax=i
+print(curmax)
---
+++ Task3.py
@@ -0,0 +1,11 @@
+dict= {True:'random',1:'value',1.0:'rishabh'}
+print(dict.get(1.0))
+sales = { 'apple': 2, 'orange': 3, 'grapes': 4 }
+element = sales.pop('apple')
+print('The popped element is:', element)
+
+lst=[32,444,2234,55443]
+lst.append(60)
+print(lst)
+lst.sort()
+print(lst)
